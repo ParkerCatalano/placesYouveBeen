e41f25023d0931802a0dd1330a73b965e07841fc
ParkerCatalano <parkercatalano@gmail.com>
2016-03-20 13:42:37 -0700

    add final script

--- js/scripts.js
+++ js/scripts.js
@@ -12,52 +12,51 @@
 // user interface logic
   $(document).ready(function() {
 
-  /$("#add-attributes").click(function() {
-//  $("#new-attributes").append('<div class="new-attribute">' +
-//                               '<div class="form-group">' +
-//                                 '<label for="new-landmark">Landmark</label>' +
-//                                 '<input type="text" class="form-control new-landmark">' +
-//                               '</div>' +
-//                               '<div class="form-group">' +
-//                                 '<label for="new-weather">Weather</label>' +
-//                                 '<input type="text" class="form-control new-weather">' +
-//                               '</div>' +
-//                               '<div class="form-group">' +
-//                                 '<label for="new-notes">Notes</label>' +
-//                                 '<input type="text" class="form-control new-notes">' +
-//                               '</div>' +
-//                             '</div>');
-//});
-//  $("form#new-place").submit(function(event) {
-///    event.preventDefault();
+    $("#add-attributes").click(function() {
+    $("#new-attributes").append('<div class="new-attribute">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-landmark">Landmark</label>' +
+                                   '<input type="text" class="form-control new-landmark">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-weather">Weather</label>' +
+                                   '<input type="text" class="form-control new-weather">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-notes">Notes</label>' +
+                                   '<input type="text" class="form-control new-notes">' +
+                                 '</div>' +
+                               '</div>');
+  });
+    $("form#new-place").submit(function(event) {
+      event.preventDefault();
 
-//    var inputtedPlaceLocation = $("input#new-place-location").val();
+      var inputtedPlaceLocation = $("input#new-place-location").val();
 
-//    var newPlace = new Place(inputtedPLaceLocation);
+      var newPlace = new Place(inputtedPlaceLocation);
 
-//    $(".new-attribute").each(function() {
-//   var inputtedLandmark = $(this).find("input.new-landmark").val();
-//   var inputtedWeather = $(this).find("input.new-weather").val();
-//   var inputtedNotes = $(this).find("input.new-notes").val();
-//   newPlace.attributes.push(newAttribute)
- //});
+      $(".new-attribute").each(function() {
+     var inputtedLandmark = $(this).find("input.new-landmark").val();
+     var inputtedWeather = $(this).find("input.new-weather").val();
+     var inputtedNotes = $(this).find("input.new-notes").val();
+     newPlace.attributes.push(newAttribute)
+  });
 
-//    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+     $("ul#attributes").append("<li><span class='new-place-location'>" + newPlace.location + " " + newPlace.attributes + "</span></li>");
 
 
-//    $(".contact").last().click(function() {
-//      $("#show-contact").show();
-//      $("#show-contact h2").text(newContact.firstName);
-//      $(".first-name").text(newContact.firstName);
-//      $(".last-name").text(newContact.lastName);
-//      $("ul#addresses").text("");
-//newContact.addresses.forEach(function(address) {
-//  $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.state + "</li>");
-//});
-//    });
+      $(".attributes").last().click(function() {
+        $("#show-place").show();
+        $("#show-place h2").text(newPlace.location);
+        $(".new-place-location").text(newPlace.location);
+        $(".new-place-landmark").text(newPlace.landmark);
+        $("ul#addresses").text("");
+  newPlace.location.forEach(function(place) {
+    $("ul#attributes").append("<li>" + attribute.landmark + attribute.season + attribute.notes + "</li>");
+  });
+      });
 
-//    $("input#new-first-name").val("");
-//    $("input#new-last-name").val("");
+      $("input#new-place-location").val("");
 
-//  });
-//});
+    });
+  });
